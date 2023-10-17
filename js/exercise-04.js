c3b16402c2c898b00c4dc45f53c4352faab9fb65
Martí Cobos <marti.cobos@mataro.epiaedu.cat>
2023-10-17 20:33:35 +0200

    EXERCICES

---
+++ js/exercise-04.js
@@ -0,0 +1,18 @@
+/*
+    What would each of these lines of code do:
+
+    1. true && console.log("moo");
+    2. false && console.log("moo moo?")
+    3. true || console.log("hello friend");
+    4. false || console.log("bye friend");
+
+    Once you see the results, write in a comment why it works that way
+*/
+
+
+//RESPONSE:
+
+true && console.log("moo");  
+false && console.log("moo moo?"); 
+true || console.log("hello friend");  
+false || console.log("bye friend");  
