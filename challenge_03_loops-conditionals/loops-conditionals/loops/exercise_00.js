bac552ef7605fb26f1e3fd3cb257cb900aa51c87
MartiCobos99 <144819011+MartiCobos99@users.noreply.github.com>
2023-10-31 19:04:03 +0100

    Exercices loops

--- challenge_03_loops-conditionals/loops-conditionals/loops/exercise_00.js
+++ challenge_03_loops-conditionals/loops-conditionals/loops/exercise_00.js
@@ -3,3 +3,17 @@
     
     2. Create a for loop that will print out all the odd numbers between 10 and 40.
 */
+
+
+/* RESPONSE */
+
+
+// for even numbers
+for (let number = 10; number <= 40; number += 2) {
+  console.log(number);
+}
+
+// fot odd numbers
+for (let number = 11; number <= 39; number += 2) {
+  console.log(number);
+}
