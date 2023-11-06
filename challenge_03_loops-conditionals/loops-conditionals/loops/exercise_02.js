3fc1dd3f70f2ad0b2b5681326eb6f9376bd55545
MartiCobos99 <144819011+MartiCobos99@users.noreply.github.com>
2023-11-06 17:28:39 +0100

    Fixed Bugs

--- challenge_03_loops-conditionals/loops-conditionals/loops/exercise_02.js
+++ challenge_03_loops-conditionals/loops-conditionals/loops/exercise_02.js
@@ -22,17 +22,13 @@
 /* RESPONSE */
 
 for (let i = 1; i <= 100; i++) {
-  
   if (i === 100) {
     console.log("You made it!");
   } else if (i === 50) {
     console.log("Half way there!");
-  }
-  else if (i % 10 === 0) {
+  } else if (i % 10 === 0) {
     console.log("Checkpoint!", i);
   }
-
-
 }
 
 console.log("All, done!");
